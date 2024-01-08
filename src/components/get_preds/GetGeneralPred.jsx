d5d43b00269da32d6e9b032305929d9c170c77bb
basra4ka <evgeha_ivanov_01@bk.ru>
2024-01-08 19:14:53 +0300

    get gen preds

--- src/components/get_preds/GetGeneralPred.jsx
+++ src/components/get_preds/GetGeneralPred.jsx
@@ -1,16 +1,20 @@
 import "../Common.css"
-import { getFromLocalStorage } from "../../utils/utils"
+import { getFromLocalStorage, removeFromLocalStorage, saveToLocalStorage, removeAll} from "../../utils/utils"
 import { useEffect, useState } from "react"
+import $ from 'jquery'
 
-const PredCard = ({text, date}) =>{
+const PredCard = ({text, inkey, update}) =>{
+
+    const deletePred = ()=>{
+        removeFromLocalStorage("general", inkey)
+        update()
+    }
 
     return(
         <div className="container-fluid prediction_holder col-7 mt-3 mb-3">
-            <span className="close-btn">&#10060;</span>
+            <span onClick={deletePred} className="close-btn">&#10060;</span>
             <div className="row justify-content-center">
-                <div className="col-12 text-start fw-bold mt-2">
-                    <span>{date}</span>
-                </div>
+
 
                 <div className="col-12 text-start mt-3 mb-2">
                     <span>{text}</span>
@@ -22,28 +26,44 @@ const PredCard = ({text, date}) =>{
 
 const GetGeneralPred = () =>{
 
-    // const [predictions, setPredictions] = useState(getFromLocalStorage("general"))
+    const [predictions, setPredictions] = useState()
 
-    const predictions = [{
-        text:"312312 выф ыфв ыфрв ыфолв оырфов рфыо вфыр воыфро вфыр овр ыфо",
-        date:"321321312312"
-    },
-    {
-        text:"312312 выф ыфв ыфрв ыфолв оырфов рфыо вфыр воыфро вфыр овр ыфо",
-        date:"321321312312"
-    },
-    {
-        text:"312312 выф ыфв ыфрв ыфолв оырфов рфыо вфыр воыфро вфыр овр ыфо",
-        date:"321321312312"
-    },
-    {
-        text:"312312 выф ыфв ыфрв ыфолв оырфов рфыо вфыр воыфро вфыр овр ыфо",
-        date:"321321312312"
-    },
-    {
-        text:"312312 выф ыфв ыфрв ыфолв оырфов рфыо вфыр воыфро вфыр овр ыфо",
-        date:"321321312312"
-    }]
+    const updatePreds = () =>{
+        const preds = getFromLocalStorage("general")
+        const predsArr = []
+        
+        for(var key in preds){
+            if (key != 'last'){
+                predsArr.push({
+                    text:preds[key],
+                    inkey:key
+                })
+            }
+        }
+
+        setPredictions(predsArr)
+    }
+
+    useEffect(()=>{
+        updatePreds()
+    }, [])
+
+    const [prediction, setPrediction] = useState("")
+
+    const getPrediction = () =>{
+        $.get("http://localhost:8080/getGeneral", (data)=>{
+            setPrediction(data)
+
+            saveToLocalStorage("general", data)
+
+            updatePreds()
+        })
+    }
+
+    const removeAllPreds = () =>{
+        removeAll("general")
+        updatePreds()
+    }
 
 
     const goBack = () =>{
@@ -51,11 +71,12 @@ const GetGeneralPred = () =>{
     }
 
 
+
     return(
         <div className="container-fluid">
             <div className="row justify-content-center text-center">
                 <div className="col-12 bazar-btn-holder">
-                    <button className="btn get-bazar-btn col-4">
+                    <button onClick={getPrediction} className="btn get-bazar-btn col-4">
                         базар судьбы
                     </button>
                 </div>
@@ -67,18 +88,24 @@ const GetGeneralPred = () =>{
                 </div>
             </div>
 
+            <div className="row justify-content-center text-center mt-5">
+                <span className="col-10 fw-bold prediction-text">
+                    {prediction}
+                </span>
+            </div>
+           
             {predictions?(
                 <div className="row justify-content-center mt-5">
                     <div className="col-12 row justify-content-center">
                         <div className="col-7">
-                        <button className="btn remove-btn col-2">Удалить все</button>
+                        <button onClick={removeAllPreds} className="btn remove-btn col-2">Удалить все</button>
                         </div>
                         
                     </div>
                     
                     <div className="col-12">
                         {predictions.map(p =>(
-                            <PredCard text={p.text} date={p.date}/>
+                            <PredCard text={p.text} inkey={p.inkey} update={updatePreds}/>
                         ))}
                     </div>
                 </div>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -12,7 +12,7 @@ export const saveToLocalStorage = (key, data) =>{
     }else{
         let next = item.last + 1
         item.last = next
-        item.next = data
+        item[next] = data
 
         localStorage.setItem(key, 
             JSON.stringify(
@@ -22,14 +22,30 @@ export const saveToLocalStorage = (key, data) =>{
 }
 
 export const removeFromLocalStorage = (key, internalKey) =>{
+    console.log("removing ", internalKey)
     let item = JSON.parse(localStorage.getItem(key))
 
     if(item != undefined){
         delete item[internalKey]
     }
 
+
+
+    for(let i=internalKey; i<item.last; i++){
+        item[i] = item[parseInt(i)+1]
+    }
+
+
+    delete item[item.last]
+
+    item.last = parseInt(item.last)-1
+    
     localStorage.setItem(key, 
         JSON.stringify(item))
+}
+
+export const removeAll = (key) =>{
+    localStorage.removeItem(key)
 }
 
 export const getFromLocalStorage = (key) =>{
