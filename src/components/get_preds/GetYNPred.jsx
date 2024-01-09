b20351a55737c2800419bafadb0e0c367fbed5b0
basra4ka <evgeha_ivanov_01@bk.ru>
2024-01-09 20:47:07 +0300

    get yn preds ready

--- src/components/get_preds/GetYNPred.jsx
+++ src/components/get_preds/GetYNPred.jsx
@@ -1,11 +1,63 @@
 import $ from 'jquery'
+import { useState } from 'react'
+import { saveToLocalStorage } from '../../utils/utils'
+
+const YNCard = ({predText, index}) =>{
+
+    const [text, setText] = useState("набазарено, но скрыто, жмякай и смотри")
+    const [flipped, setFlipped] = useState(false)
+
+    const flip = () =>{
+        
+        if(!flipped){
+            document.getElementById(`yn-holder-${index}`).animate([
+                {transform:'rotateY(90deg)'}
+            ], {
+                duration: 300,
+                iterations: 1
+            })
+            .onfinish =()=>{
+                $(`#yn-holder-${index}`).removeClass("before-flip-yn-holder")
+                setText(predText)
+            }
+            setFlipped(true)
+        }
+        
+    }
+
+    return(
+        <div onClick={flip} id={`yn-holder-${index}`} className="col-3 prediction_holder yn-holder pt-2 pb-2 before-flip-yn-holder">
+            <span>{text}</span>
+        </div>
+    )
+}
+
+const YNPred = ({preds})=>{
+
+
+    return(
+        <div className="container-fluid mt-5">
+            <div className="row justify-content-center text-center">
+                {preds.map((pred, index) =>(
+                    <YNCard predText={pred} index={index}/>
+                ))}
+            </div>
+        </div>
+    )
+}
 
 const GetYNPred = () =>{
 
+    const [predictions, setPredictions] = useState(undefined)
+    const [status, setStatus] = useState("тут будет судьба")
+
     const getPrediction = () =>{
+        setPredictions(undefined)
+        setStatus("гадаем судьбу...")
         $.get("http://localhost:8080/getYN", (data)=>{
-            // console.log("get pred after fetch inside")
-            console.log(data)
+            saveToLocalStorage("yn", data)
+
+            setPredictions(data)
         })
     }
 
@@ -29,6 +81,11 @@ const GetYNPred = () =>{
                 </div>
             </div>
 
+            {predictions?(<YNPred preds={predictions}/>):
+                <div className='row justify-content-center text-center mt-5'>
+                    <span style={{color:"#ffffff"}} className='fw-bold'>{status}</span>
+                </div>
+            }
             
         </div>
     )
